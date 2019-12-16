0315d21ead03c625da335cefbe1e7c1d28eb5d3c
00day0 <therandomuser11@gmail.com>
2019-12-16 21:31:27 +0100

    SystemUI: FOD icon picker [1/2]
    Co-authored-by: Stallix <thestallix@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: If14030748b76a492bed199de4280cf8ec4dbea38

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5885,6 +5885,12 @@ public final class Settings {
         public static final String QS_HEADER_STYLE = "qs_header_style";
 
         /**
+         * FOD icon picker
+         * @hide
+         */
+        public static final String FOD_ICON = "fod_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
